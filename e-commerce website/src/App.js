540f3d24e4831e673707e13a017d5764c315941d
Shivang Baranwal <91139730+Shivang1179793@users.noreply.github.com>
2024-01-11 23:52:16 +0530

    Update App.js

--- e-commerce website/src/App.js
+++ e-commerce website/src/App.js
@@ -4,10 +4,7 @@ import NavBar from './components/e-commerce/navbar/NavBar'
 function App() {
   return (
     <div className="App">
-      {/* <Child1></Child1>
-      <Child2></Child2> */}
-      {/* <CreateNote />
-      <ListNotes /> */}
+      
       <NavBar />
       <ProductList />
     </div>
