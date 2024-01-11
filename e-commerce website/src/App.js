454e9dc6f477ab232671fb8234cff22080e77f25
Shivang Baranwal <91139730+Shivang1179793@users.noreply.github.com>
2024-01-11 23:51:52 +0530

    Update App.js

--- e-commerce website/src/App.js
+++ e-commerce website/src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import Child1 from './components/Child1';
-import Child2 from './components/Child2';
 import ProductList from './components/e-commerce/productList/ProductList';
-import CreateNote from "./components/notes/CreateNote";
-import ListNotes from "./components/notes/ListNotes";
 import NavBar from './components/e-commerce/navbar/NavBar'
 function App() {
   return (
