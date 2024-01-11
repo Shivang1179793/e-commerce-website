30e047a44373c5dd07c9ff90219315dadacc6cea
Shivang Baranwal <91139730+Shivang1179793@users.noreply.github.com>
2024-01-11 23:54:48 +0530

    Update store.js

--- e-commerce website/src/redux/store.js
+++ e-commerce website/src/redux/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import messageReducer from "./slices/messageSlice";
-import notesSlice from "./slices/notesSlice";
 import productReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartSlice";
 export default configureStore({
     reducer: {
-        // messageReducer,
-        // notesReducer: notesSlice
         productReducer,
         cartReducer
     }
